7d56f67f7cf5d9543b91f799d348536fda05ef5c
kostyálpéter <kostyal.peter123@gmail.com>
2025-10-23 13:48:25 +0200

    player, player movement, facing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private bool facingRight;
+
+    private SpriteRenderer sprite;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // mozgás input
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
+
+        // player flip (balra/jobbra)
+        if (moveX > 0 && !facingRight)
+        {
+            facingRight = true;
+            sprite.flipX = false;
+        }
+        else if (moveX < 0 && facingRight)
+        {
+            facingRight = false;
+            sprite.flipX = true;
+        }
+
+        // animáció paraméterek
+        animator.SetFloat("moveX", moveX);
+        animator.SetFloat("moveY", moveY);
+        animator.SetBool("IsMoving", moveInput != Vector2.zero);
+    }
+
+    void FixedUpdate()
+    {
+        if (rb != null)
+        {
+            rb.linearVelocity = moveInput * moveSpeed;
+        }
+    }
+}
