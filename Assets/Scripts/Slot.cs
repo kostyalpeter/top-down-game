6d4d9e6f1831e8bda9bad48fd626f664e241c6c8
kostyálpéter <kostyal.peter123@gmail.com>
2025-11-17 18:24:00 +0100

    bug fix in progress

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -1,17 +1,24 @@
+using System.Collections;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Slot : MonoBehaviour
 
 {
-
     public void UseItem()
     {
         IItem item = currentItem.GetComponent<IItem>();
         if (item != null)
         {
             item.Use();
-            currentItem.GetComponent<Item>().quantity -= 1;
+            currentItem.GetComponent<Item>().RemoveFromStack(1);
         }
+        if (item == null)
+        {
+            Destroy(currentItem);
+
+        }
+
     }
     public GameObject currentItem;
 }
