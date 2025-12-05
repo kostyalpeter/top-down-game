713827cd11a50baa0872c9b974b6cf25880a6a01
kostyálpéter <kostyal.peter123@gmail.com>
2025-12-05 20:12:13 +0100

    shop area done, new teleport area in porgress, teleport done

---
+++ Assets/Scripts/Teleport/Teleport4.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Teleport4 : MonoBehaviour
+
+{
+    [SerializeField] private float interactDistance = 2f;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        float dist = Vector2.Distance(transform.position, player.position);
+
+        if (dist <= interactDistance && Input.GetKeyDown(KeyCode.C))
+        {
+            TeleportPlayer();
+        }
+    }
+
+    private void TeleportPlayer()
+    {
+        player.transform.position = new Vector3((float)190.6, (float)310.93, 0);
+    }
+}
