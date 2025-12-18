3e0768d6921cda93f241e515177a9eb1a51a1f76
kostyálpéter <kostyal.peter123@gmail.com>
2025-12-18 15:59:25 +0100

    new borders, runes in progress

---
+++ Assets/Scripts/Rune.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Rune : MonoBehaviour
+{
+    public int RuneInt = 0;
+    void Update()
+    {
+        if (RuneInt == 4)
+        {
+            Debug.Log("asdasdasdasdasdasdsadas");
+        }
+    }
+ond
+}
