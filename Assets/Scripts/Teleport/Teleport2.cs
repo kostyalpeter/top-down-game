f4699dc5da2cd328b33fa61e65065911f497a011
kostyálpéter <kostyal.peter123@gmail.com>
2025-12-20 17:14:40 +0100

    tested the game, fixed bugs

--- Assets/Scripts/Teleport/Teleport2.cs
+++ Assets/Scripts/Teleport/Teleport2.cs
@@ -27,6 +27,6 @@ public class Teleport2 : MonoBehaviour
 
     private void TeleportPlayer()
     {
-        player.transform.position = new Vector3((float)424.17, (float)250.44, 0);
+        player.transform.position = new Vector3((float)202.25, (float)-347.79, 0);
     }
 }
